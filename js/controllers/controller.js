12a57f8aa680bdea6a9f06cbf6dbca552b111b7f
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-03-29 11:59:23 +0200

    Update controller.js

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,5 +1,6 @@
 import { Subject } from "../patterns/subject.js";
 import { NONE, MAINMENU, GAME, LEVELSMANAGEMENTMENU } from "../views/viewType.js";
+import { Level } from "../models/level.js";
 
 export class Controller extends Subject
 {
@@ -12,7 +13,7 @@ export class Controller extends Subject
         super();
         this.#currentScreen = MAINMENU;
         this.#maps = [];
-        this.#maps.push(this.getMap("/maps/test.txt"));
+        this.addMap("/maps/test.txt");
     }
 
     set currentScreen(value) { this.#currentScreen = value; this.notify(); }
@@ -21,7 +22,7 @@ export class Controller extends Subject
     set level(value) { this.#level = value; }
     get level() { return this.#level; }
 
-    getMap(address)
+    addMap(address)
     {
         fetch(address).then((res) => res.text()).then((text) => {
             const tab = text.split(/\r\n/m);
@@ -29,14 +30,15 @@ export class Controller extends Subject
             tab.forEach((line, i) => {
                 map.push(line.split(""));
             });
-            return map;
+            this.#maps.push(map);
         });
     }
 
     newGame()
     {
-        console.log(this.getMap("/maps/test.txt"));
         console.log(this.#maps[0]);
+        this.#level = new Level(this.#maps[0]);
+        console.log(this.#level);
         this.currentScreen = GAME;
     }
 }
