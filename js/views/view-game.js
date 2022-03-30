c8201f9ad86166b1b8d767b3fa528b848a4f0d55
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-03-30 19:21:02 +0200

    Update view-game.js

--- js/views/view-game.js
+++ js/views/view-game.js
@@ -1,3 +1,4 @@
+import { WALL } from "../models/block.js";
 import { View } from "./view.js";
 import { GAME } from "./viewType.js";
 
@@ -14,5 +15,24 @@ export class ViewGame extends View
     {
         const appHTML = document.querySelector("app");
         appHTML.innerHTML = "";
+
+        const level = this._controller.level;
+
+        const gridHTLM = document.createElement("game");
+
+        level.grid.forEach((line, i) => {
+            const lineHTML = document.createElement("div");
+
+            line.forEach((block, j) => {
+                const blockHTML = document.createElement("div");
+                blockHTML.classList.add(block.type);
+                blockHTML.textContent = block.type;
+                lineHTML.appendChild(blockHTML);
+            });
+
+            gridHTLM.appendChild(lineHTML);
+        });
+
+        appHTML.appendChild(gridHTLM);
     }
 }
