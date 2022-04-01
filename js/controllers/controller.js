465a28e50cf15e311e842d282d0cd4f587d4e865
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-01 20:40:42 +0200

    Update controller.js

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,12 +1,14 @@
 import { Subject } from "../patterns/subject.js";
 import { NONE, MAINMENU, GAME, LEVELSMANAGEMENTMENU } from "../views/viewType.js";
 import { Level } from "../models/level.js";
+import { UP, LEFT, DOWN, RIGHT } from "../models/direction.js"
 
 export class Controller extends Subject
 {
     #currentScreen;
     #level;
     #maps;
+    #mapNumber;
 
     constructor()
     {
@@ -14,6 +16,7 @@ export class Controller extends Subject
         this.#currentScreen = MAINMENU;
         this.#maps = [];
         this.addMap("/maps/test.txt");
+        this.#mapNumber = 0;
     }
 
     set currentScreen(value) { this.#currentScreen = value; this.notify(); }
@@ -35,9 +38,51 @@ export class Controller extends Subject
 
     newGame()
     {
-        console.log(this.#maps[0]);
-        this.#level = new Level(this.#maps[0]);
-        console.log(this.#level);
+        this.#mapNumber = 0;
+        this.#level = new Level(this.#maps[this.#mapNumber]);
         this.currentScreen = GAME;
+    }
+
+    keyDown(event)
+    {
+        if (this.currentScreen === GAME)
+        {
+            switch (event.key)
+            {
+                case 'z':
+                    this.#level.move(UP);
+                    this.notify();
+                    break;
+                
+                case 'q':
+                    this.#level.move(LEFT);
+                    this.notify();
+                    break;
+                    
+                case 's':
+                    this.#level.move(DOWN);
+                    this.notify();
+                    break;
+                        
+                case 'd':
+                    this.#level.move(RIGHT);
+                    this.notify();
+                    break;
+
+                default:
+                    break;                      
+            }
+
+            if (this.#level.isWin())
+            {
+                console.log("Its win!");
+                //win screen (next level or return menu)
+            }
+            else if (this.#level.isLoose())
+            {
+                console.log("Its loose!");
+                //loose screen (retry level or return menu)
+            }
+        }
     }
 }
