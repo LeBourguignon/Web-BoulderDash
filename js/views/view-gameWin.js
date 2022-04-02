b630e060746c099b3f280ba9defc05ce1dd518ae
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-02 23:51:32 +0200

    Create view-gameWin.js

---
+++ js/views/view-gameWin.js
@@ -0,0 +1,43 @@
+import { View } from "./view.js";
+import { GAMEWIN } from "./viewType.js";
+
+export class ViewGameWin extends View
+{
+    constructor(controller)
+    {
+        super(controller);
+        this._viewType = GAMEWIN;
+        this.notify();
+    }
+
+    update()
+    {
+        const appHTML = document.querySelector("app");
+
+        const winHTML = document.createElement("div");
+
+        const titleHTML = document.createElement("h1");
+        titleHTML.textContent = "Félitation, tu as gagné la partie !";
+        winHTML.appendChild(titleHTML);
+
+        const buttonHTML = document.createElement("div");
+
+        const buttonRMM = document.createElement("button");
+        buttonRMM.textContent = "Retour au menu principal";
+        buttonRMM.addEventListener("click", () => {
+            this._controller.returnMainMenu();
+        });
+        buttonHTML.appendChild(buttonRMM);
+
+        const buttonRL = document.createElement("button");
+        buttonRL.textContent = "Recommencer le niveau";
+        buttonRL.addEventListener("click", () => {
+            this._controller.restartLevel();
+        });
+        buttonHTML.appendChild(buttonRL);
+
+        winHTML.appendChild(buttonHTML);
+
+        appHTML.appendChild(winHTML);
+    }
+}
