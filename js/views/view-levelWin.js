ff4408f2ba25f3df1de11d964d6c7855dd051e4b
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-02 23:51:11 +0200

    Create view-levelWin.js

---
+++ js/views/view-levelWin.js
@@ -0,0 +1,50 @@
+import { View } from "./view.js";
+import { LEVELWIN } from "./viewType.js";
+
+export class ViewLevelWin extends View
+{
+    constructor(controller)
+    {
+        super(controller);
+        this._viewType = LEVELWIN;
+        this.notify();
+    }
+
+    update()
+    {
+        const appHTML = document.querySelector("app");
+
+        const winHTML = document.createElement("div");
+
+        const titleHTML = document.createElement("h1");
+        titleHTML.textContent = "Niveau " + this._controller.mapNumber + " gagné !";
+        winHTML.appendChild(titleHTML);
+
+        const buttonHTML = document.createElement("div");
+
+        const buttonRMM = document.createElement("button");
+        buttonRMM.textContent = "Retour au menu principal";
+        buttonRMM.addEventListener("click", () => {
+            this._controller.returnMainMenu();
+        });
+        buttonHTML.appendChild(buttonRMM);
+
+        const buttonRL = document.createElement("button");
+        buttonRL.textContent = "Recommencer le niveau";
+        buttonRL.addEventListener("click", () => {
+            this._controller.restartLevel();
+        });
+        buttonHTML.appendChild(buttonRL);
+
+        const buttonNL = document.createElement("button");
+        buttonNL.textContent = "Niveau suivant";
+        buttonNL.addEventListener("click", () => {
+            this._controller.nextLevel();
+        });
+        buttonHTML.appendChild(buttonNL);
+
+        winHTML.appendChild(buttonHTML);
+
+        appHTML.appendChild(winHTML);
+    }
+}
