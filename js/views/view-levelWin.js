c64f8697272d1ddf9db7087c70202ddebcb7662e
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-09 21:25:44 +0200

    Update view-levelWin.js

--- js/views/view-levelWin.js
+++ js/views/view-levelWin.js
@@ -15,6 +15,10 @@ export class ViewLevelWin extends View
 		const gameFooter = document.getElementById("gameFooter");
 		gameFooter.innerHTML = "";
 
+        const lastConfirmationHTML = document.querySelector("confirmation");
+        if (lastConfirmationHTML !== null)
+            lastConfirmationHTML.remove();
+
         const appHTML = document.querySelector("app");
 
         const winHTML = document.createElement("confirmation");
