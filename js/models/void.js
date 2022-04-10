5ba411d6b1aa35a8a8d884b5d6f138013f35624a
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 14:49:56 +0200

    Update void.js

--- js/models/void.js
+++ js/models/void.js
@@ -4,11 +4,20 @@ import { Level } from "./level.js";
 
 export class Void extends Block
 {
+    /**
+     * Constructor
+     * @param {Level} level : Level in which the block is located
+     * @param {Coordinate} coordinate : Block cordinate
+     */
     constructor(level, coordinate)
     {
         super(VOID, level, coordinate);
     }
 
+    /**
+     * Returns whether the block can be destroyed
+     * @returns {boolean} : A boolean where true is destructible
+     */
     isDestructible()
     {
         return true;
