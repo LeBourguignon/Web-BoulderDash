b2b3d232351fa32dc035d42122d0893185d73547
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 14:49:59 +0200

    Update wall.js

--- js/models/wall.js
+++ js/models/wall.js
@@ -4,11 +4,20 @@ import { Level } from "./level.js";
 
 export class Wall extends Block
 {
+    /**
+     * Constructor
+     * @param {Level} level : Level in which the block is located
+     * @param {Coordinate} coordinate : Block cordinate
+     */
     constructor(level, coordinate)
     {
         super(WALL, level, coordinate);
     }
 
+    /**
+     * Returns whether the block can be destroyed
+     * @returns {boolean} : A boolean where true is destructible
+     */
     isDestructible()
     {
         return false;
