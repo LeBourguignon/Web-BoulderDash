45a5f1a9ae958203c6ade4143fa07c157d99cb21
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 15:59:07 +0200

    Update view.js

--- js/views/view.js
+++ js/views/view.js
@@ -1,11 +1,19 @@
 import { Observer } from "../patterns/observer.js";
 import { NONE } from "./viewType.js";
+import { Controller } from "../controllers/controller.js";
 
 export class View extends Observer
 {
+    //Controller managing the view
     _controller;
+
+    //View type
     _viewType;
 
+    /**
+     * constructor
+     * @param {Controller} controller : Controller managing the view
+     */
     constructor(controller)
     {
         super();
@@ -16,17 +24,27 @@ export class View extends Observer
         this._viewType = NONE;
     }
 
+    /**
+     * Checks that the view is the one displayed by the controller
+     * @returns {boolean}
+     */
     checkView()
     {
         return this._controller.currentScreen === this._viewType;
     }
 
+    /**
+     * Notify the view
+     */
     notify()
     {
         if (this.checkView())
             this.update();
     }
 
+    /**
+     * update of the view
+     */
     update()
     {
         throw "Redefine the update method!";
