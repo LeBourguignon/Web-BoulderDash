72f70e4dbd8caae70918ba375e245904dd9786c9
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 15:58:08 +0200

    Update observer.js

--- js/patterns/observer.js
+++ js/patterns/observer.js
@@ -1,5 +1,9 @@
 export class Observer
 {
+    /**
+     * /!\ Abstract method - Must be redefined /!\
+     * Notify the view
+     */
     notify()
     {
         throw "Redefine the notify method!";
