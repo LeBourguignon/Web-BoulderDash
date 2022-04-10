3fe23e23632ed6307665ab9102de347e98849f5b
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 14:49:54 +0200

    Update rock.js

--- js/models/rock.js
+++ js/models/rock.js
@@ -4,8 +4,14 @@ import { Level } from "./level.js";
 
 export class Rock extends Block
 {
+    //If the rock is falling
     #isMoving;
 
+    /**
+     * Constructor
+     * @param {Level} level : Level in which the block is located
+     * @param {Coordinate} coordinate : Block cordinate
+     */
     constructor(level, coordinate)
     {
         super(ROCK, level, coordinate);
@@ -15,6 +21,10 @@ export class Rock extends Block
     get isMoving() { return this.#isMoving; }
     set isMoving(value) { this.#isMoving = value; }
 
+     /**
+     * Returns whether the block can be destroyed
+     * @returns {boolean} : A boolean where true is destructible
+     */
     isDestructible()
     {
         return false;
