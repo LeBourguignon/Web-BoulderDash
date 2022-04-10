9716306b2c9922419c3b8373e2e44f46a2b4134d
LeBourguignon <63340452+LeBourguignon@users.noreply.github.com>
2022-04-10 14:49:47 +0200

    Update dirt.js

--- js/models/dirt.js
+++ js/models/dirt.js
@@ -4,11 +4,20 @@ import { Level } from "./level.js";
 
 export class Dirt extends Block
 {
+    /**
+     * Constructor
+     * @param {Level} level : Level in which the block is located
+     * @param {Coordinate} coordinate : Block cordinate
+     */
     constructor(level, coordinate)
     {
         super(DIRT, level, coordinate);
     }
 
+    /**
+     * Returns whether the block can be destroyed
+     * @returns {boolean} : A boolean where true is destructible
+     */
     isDestructible()
     {
         return true;
